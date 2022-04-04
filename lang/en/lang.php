500da909e4317912413ad9575cf56e3804bc77b9
Marc Jauvin <marc.jauvin@gmail.com>
2022-04-04 13:50:35 -0400

    add french localizations

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,12 @@
 <?php
 
-$name = 'Postmark secret';
-
 return [
     'plugin_description' => 'Postmark mail driver plugin',
 
     'fields' => [
         'postmark_secret' => [
-            'label' => $name,
-            'comment' => 'Enter your ' . $name,
+            'label' => 'Postmark secret',
+            'comment' => 'Enter your Postmark secret',
         ],
     ],
 ];
---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'plugin_description' => "Plugin qui ajoute l'envoi de courriel à travers Postmark",
+
+    'fields' => [
+        'postmark_secret' => [
+            'label' => 'Secret Postmark',
+            'comment' => 'Entrez votre secret Postmark',
+        ],
+    ],
+];
