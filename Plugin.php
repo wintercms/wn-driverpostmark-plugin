5cec6e5bc3b50ae6c49cb676a4cffb5270315499
Marc Jauvin <marc.jauvin@gmail.com>
2022-04-06 14:17:09 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -10,6 +10,8 @@ use System\Models\MailSetting;
  */
 class Plugin extends PluginBase
 {
+    public $elevated = true;
+    
     const MODE_POSTMARK = 'postmark';
 
     public function pluginDetails()
